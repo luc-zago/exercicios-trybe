dbeaabb3e7c3f00f368e04a995456cabf73b0aaa
luc-zago <luc.zago@hotmail.com>
2020-08-27 18:19:59 -0300

    Criado programa 'verifica-angulos-internos-triangulo' que, dado os três ângulos internos de um objeto, devolve 'true' caso o objeto seja um triângulo e 'false' caso o objeto não seja um triângulo

---
+++ 4.1/verifica-angulos-internos-triangulo.js
@@ -0,0 +1,9 @@
+//Descobre se um objeto é um triângulo a partir dos valores de seus ângulos internos
+let a = 50
+let b = 30
+let c = 101
+
+if ((a + b + c) == 180) {
+    console.log('true') }
+    else
+        console.log('false')
