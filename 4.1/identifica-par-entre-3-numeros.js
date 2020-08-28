482af46788e58dd632f0bcc24939c2228c541160
luc-zago <luc.zago@hotmail.com>
2020-08-28 18:28:24 -0300

    Criado programa 'identifica-par-entre-3-numeros' que recebe três números em variáveis diferentes e caso um deles seja par, retorna 'true'. Caso nenhum deles seja par, retorna 'false'

---
+++ 4.1/identifica-par-entre-3-numeros.js
@@ -0,0 +1,12 @@
+//Recebe três números em uma variável e identifica se existe um par entre eles
+
+let numero1 = 3
+let numero2 = 0
+let numero3 = 7
+
+if ((numero1%2==0) || (numero2%2==0) || (numero3%2==0)) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
