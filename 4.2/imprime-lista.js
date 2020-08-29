5eff72c20f46537838d2c4809e6977b1a7924305
luc-zago <luc.zago@hotmail.com>
2020-08-28 21:00:58 -0300

    Alterado programa 'imprime-lista', alterando o código para incluir a condicional 'for', e criado programa 'soma-lista' que soma os itens de uma lista

--- 4.2/imprime-lista.js
+++ 4.2/imprime-lista.js
@@ -1,14 +1,7 @@
 //Imprime a lista 'numbers'
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 
-console.log(numbers[0])
-console.log(numbers[1])
-console.log(numbers[2])
-console.log(numbers[3])
-console.log(numbers[4])
-console.log(numbers[5])
-console.log(numbers[6])
-console.log(numbers[7])
-console.log(numbers[8])
-console.log(numbers[9])
+
+for ( let contador =0; contador < numbers.length; contador=contador+1 ) { console.log(numbers[contador])
+    }
