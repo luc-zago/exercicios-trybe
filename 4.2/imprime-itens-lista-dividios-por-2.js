1599aef2eb4cc9b775ab1171734e14e0164bb5a3
luc-zago <luc.zago@hotmail.com>
2020-08-28 21:39:04 -0300

    Criado programa 'imprime-itens-lista-divididos-por-2' que imprime os itens de uma lista divididos por 2

--- 4.2/imprime-itens-lista-dividios-por-2.js
+++ 4.2/imprime-itens-lista-dividios-por-2.js
@@ -3,5 +3,5 @@
 sequencia1a25 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 
-for ( let contador =0; contador < sequencia1a25.length; contador=contador+1 ) { console.log(sequencia1a25[contador])
+for ( let contador =0; contador < sequencia1a25.length; contador=contador+1 ) { console.log(sequencia1a25[contador] / 2 )
     }
