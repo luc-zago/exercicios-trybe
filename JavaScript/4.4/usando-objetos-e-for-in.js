afc11b6378e09ccbddf3c27bf08ab353e40c58e2
luc-zago <luc.zago@hotmail.com>
2020-09-02 17:28:09 -0300

    Adicionado o objeto 'info_patinhas' com informações sobre o Tio Patinhas

--- JavaScript/4.4/usando-objetos-e-for-in.js
+++ JavaScript/4.4/usando-objetos-e-for-in.js
@@ -6,6 +6,13 @@ let info = {
     recorrente: "Sim"
 }
 
+let info_patinhas = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+/*
 console.log('Bem-vinda, ' + info.personagem)
 
 console.log(info)
@@ -16,4 +23,4 @@ for (let key in info) {
 
 for (let value in info) {
     console.log(info[value]);
-}
+} */
