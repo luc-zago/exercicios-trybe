a4572a1c11a085484030259cae7030806b5bf8fd
luc-zago <luc.zago@hotmail.com>
2020-09-02 16:41:53 -0300

    Finalizado programa 'imprime-piramide' que, dado um parâmetro 'n', o programa retorna uma pirâmide de base 'n'. Porém, para o programa funcionar, o parâmetro 'n' deve ser ímpar. Se o parâmetro for par, o programa retorna uma mensagem de erro

--- JavaScript/4.3/imprime-piramide.js
+++ JavaScript/4.3/imprime-piramide.js
@@ -1,41 +1,29 @@
-//Dado um número 'n', imprime uma pirâmide
+//Dado um número 'n', imprime uma pirâmide. O número n deve ser ímpar
 
-let n = 7
-/* let lista = []
+let n = 5
+let lista = []
 let listaEspaco = []
-*/
 
 if (n%2==0) {
-    console.log('par')
-}
+    console.log('error')
+    }
+
 else {
     numeroTopoEspacoVazioPiramide = (n / 2) - 0.5
-    for (i=0; y < numeroTopoEspacoVazioPiramide; y = y+1) {
+    for (i=0; i < numeroTopoEspacoVazioPiramide; i = i+1) {
         listaEspaco.push(" ")
         listaEmStringEspaco = listaEspaco.join('');
     }
 
-// em relação aos números ímpares existe uma PA de +1
-// para cada novo ímpar, o número de linhas aumenta em 1
-// para cada novo ímpar, o número de espaços do topo da pirâmide aumenta em 1
-// o número de espaços da do topo para a base da pirâmide diminui em 1
-
-// duas listas, uma de espaços vazios e uma com '*'
-// lista de espaços vazios diminui sempre em 1 por linha
-// lista de '*' diminui sempre em 1 por linha
-
-}
-
-
-/*
-for (y=0; y < (n-1); y = y+1) {
-    listaEspaco.push(" ")
-    listaEmStringEspaco = listaEspaco.join('');
-}
-
-for (i=0; i < n; i= i+1) {
     lista.push('*')
     let listaEmString = lista.join('');
     console.log(listaEmStringEspaco, listaEmString)
-    listaEmStringEspaco = listaEmStringEspaco.substring(0, listaEmStringEspaco.length - 1);
-} */
+
+    for (y=2; y < n; y= y+2) {
+        lista.push('*')
+        lista.push('*')
+        listaEmString = lista.join('');
+        listaEmStringEspaco = listaEmStringEspaco.substring(0, listaEmStringEspaco.length - 1);
+        console.log(listaEmStringEspaco, listaEmString);
+    }
+}
