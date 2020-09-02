4155ef553608982c68d9ef772966145a846b67b7
luc-zago <luc.zago@hotmail.com>
2020-09-02 15:04:34 -0300

    Criado programa 'imprime-triangulo-retangulo' que, dado um parâmetro 'n', imprime um triângulo retângulo com '*' no topo e (n x '*') na base

---
+++ JavaScript/4.3/imprime-triangulo-retangulo.js
@@ -0,0 +1,10 @@
+//Dado um número 'n', imprime um quadrado
+
+let n = 5
+let lista = []
+
+for (i=0; i < n; i= i+1) {
+    lista.push('*')
+    let listaEmString = lista.join('');
+    console.log(listaEmString)
+}
