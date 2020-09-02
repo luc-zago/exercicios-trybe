3171c306f4bb73fb7b699a62b940037953e86381
luc-zago <luc.zago@hotmail.com>
2020-09-02 16:02:54 -0300

    Divididos os parâmetros da pirâmide, inicialmente, em pares e ímpares usando a condicional 'if'. Feitos comentários sobre o problema em relação aos números ímpares

--- JavaScript/4.3/imprime-piramide.js
+++ JavaScript/4.3/imprime-piramide.js
@@ -1,9 +1,23 @@
-//Dado um número 'n', imprime um quadrado
+//Dado um número 'n', imprime uma pirâmide
 
-let n = 5
-let lista = []
+let n = 6
+/* let lista = []
 let listaEspaco = []
+ */
 
+if (n%2==0) {
+    console.log('par')
+}
+else {
+    console.log('impar')
+// em relação aos números ímpares existe uma PA de +1
+// para cada novo ímpar, o número de linhas aumenta em 1
+// para cada novo ímpar, o número de espaços do topo da pirâmide aumenta em 1
+// o número de espaços da do topo para a base da pirâmide diminui em 1
+}
+
+
+/*
 for (y=0; y < (n-1); y = y+1) {
     listaEspaco.push(" ")
     listaEmStringEspaco = listaEspaco.join('');
@@ -14,4 +28,4 @@ for (i=0; i < n; i= i+1) {
     let listaEmString = lista.join('');
     console.log(listaEmStringEspaco, listaEmString)
     listaEmStringEspaco = listaEmStringEspaco.substring(0, listaEmStringEspaco.length - 1);
-}
+} */
