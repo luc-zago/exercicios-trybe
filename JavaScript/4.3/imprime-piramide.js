e13553b14bebca12c30664b663c5176232b67f37
luc-zago <luc.zago@hotmail.com>
2020-09-02 16:18:27 -0300

    Criado no programa 'imprime-piramide' condição para montar a lista de espaços vazios que servirá para formar o topo da pirâmide

--- JavaScript/4.3/imprime-piramide.js
+++ JavaScript/4.3/imprime-piramide.js
@@ -1,19 +1,29 @@
 //Dado um número 'n', imprime uma pirâmide
 
-let n = 6
+let n = 7
 /* let lista = []
 let listaEspaco = []
- */
+*/
 
 if (n%2==0) {
     console.log('par')
 }
 else {
-    console.log('impar')
+    numeroTopoEspacoVazioPiramide = (n / 2) - 0.5
+    for (i=0; y < numeroTopoEspacoVazioPiramide; y = y+1) {
+        listaEspaco.push(" ")
+        listaEmStringEspaco = listaEspaco.join('');
+    }
+
 // em relação aos números ímpares existe uma PA de +1
 // para cada novo ímpar, o número de linhas aumenta em 1
 // para cada novo ímpar, o número de espaços do topo da pirâmide aumenta em 1
 // o número de espaços da do topo para a base da pirâmide diminui em 1
+
+// duas listas, uma de espaços vazios e uma com '*'
+// lista de espaços vazios diminui sempre em 1 por linha
+// lista de '*' diminui sempre em 1 por linha
+
 }
 
 
