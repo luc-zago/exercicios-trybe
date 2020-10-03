3694f029122b4670b2ebe0dbc7e7d21081c17340
luc-zago <luc.zago@hotmail.com>
2020-10-02 21:05:18 -0300

    Alterando arquivos

--- HTML, CSS e JavaScript/6.2/script.js
+++ HTML, CSS e JavaScript/6.2/script.js
@@ -11,8 +11,33 @@ window.onload = function () {
       document.getElementById('user-state').appendChild(listItem);
     }
   }
-  
-  document.getElementById('date').DatePickerX.init();
-  window.DatePickerX.setDefaults();
-
 }
+
+function verifyDate() {
+  const date = document.getElementById('date').value;
+  const dateToArray = date.split('');
+  day = parseInt(dateToArray[0] + dateToArray[1]);
+  month = parseInt(dateToArray[3] + dateToArray[4]);
+  year = parseInt(dateToArray[6] + dateToArray[7] + dateToArray[8] + dateToArray[9]);
+  if (dateToArray[2] !== '/') {
+    if (dateToArray[5] !== '/') {
+    alert('Formato de data deve ser dd/mm/aaaa')
+  }
+  }
+  if (1 > day) {
+    alert('Dia não pode ser menor que 1')
+    }
+  if (day > 31) {
+    alert('Dia não pode ser maior que 31')
+  }
+  if (1 > month) {
+    alert('Mês não pode ser menor que 1')
+  }
+  if (month > 12) {
+    alert('Mês não pode ser maior que 12')
+  }
+  if (year < 0) {
+    alert('Ano não pode ser menor que 0')
+  }
+}
+
