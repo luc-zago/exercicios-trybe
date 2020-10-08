68153869f55098db16cbeb35498a30d131a6d09a
luc-zago <luc.zago@hotmail.com>
2020-10-08 11:11:31 -0300

    Finalizado exercício 4

---
+++ Bloco 8/8.4/exercicio4.js
@@ -0,0 +1,41 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+
+const filterPeople = (array) => {
+  // Resolução 1 com filter
+  return array.filter((people) => (people.nationality === 'Australian' && (people.bornIn < 2001) && (people.bornIn > 1900)));
+}
+
+const filteredPeople = filterPeople(people)
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
